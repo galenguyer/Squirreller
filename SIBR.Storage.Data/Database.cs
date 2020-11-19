82948faf5a42be337f799a204bc8d1b259f94197
Galen Guyer <galen@galenguyer.com>
2020-11-19 15:16:30 -0500

    Add mongo and clean up Database a bit

--- SIBR.Storage.Data/Database.cs
+++ SIBR.Storage.Data/Database.cs
@@ -6,6 +6,8 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Dapper;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDb.Bson.NodaTime;
+using MongoDB.Driver;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using NodaTime;
@@ -20,118 +22,32 @@ namespace SIBR.Storage.Data
 
         public static void Init()
         {
-            NpgsqlConnection.GlobalTypeMapper.UseJsonNet().UseNodaTime();
-            DefaultTypeMap.MatchNamesWithUnderscores = true;
-            SqlMapper.AddTypeHandler(new PassthroughTypeHandler<Instant>(NpgsqlDbType.TimestampTz));
-            SqlMapper.AddTypeHandler(new JsonTypeHandler<JToken>());
-            SqlMapper.AddTypeHandler(new JsonTypeHandler<JValue>());
-            SqlMapper.AddTypeHandler(new JsonTypeHandler<JArray>());
-            SqlMapper.AddTypeHandler(new JsonTypeHandler<JObject>());
-            SqlMapper.AddTypeHandler(new JsonTypeHandler<JObject>());
-            SqlMapper.AddTypeHandler(new STJsonTypeHandler());
+            NodaTimeSerializers.Register();
         }
 
         public Database(IServiceProvider services, string connectionString)
         {
             _logger = services.GetRequiredService<ILogger>().ForContext<Database>();
-            _connectionString = new NpgsqlConnectionStringBuilder(connectionString)
-                {
-                    Enlist = false,
-                    NoResetOnClose = true,
-                    WriteBufferSize = 1024*64
-                }
-                .ConnectionString;
+            _connectionString = connectionString;
         }
 
-        public async Task<NpgsqlConnection> Obtain()
+        public async Task<MongoClient> Obtain()
         {
-            var connection = new NpgsqlConnection(_connectionString);
-            await connection.OpenAsync();
+            var connection = new MongoClient(_connectionString);
             return connection;
         }
-        
-        public async Task RefreshMaterializedViews(NpgsqlConnection conn, params string[] matViews)
-        {
-            foreach (var matView in matViews)
-            {
-                var sw = new Stopwatch();
-                sw.Start();
-                await conn.ExecuteAsync($"refresh materialized view concurrently {matView}");
-                sw.Stop();
 
-                _logger.Information("Refreshed materialized view {ViewName} in {Duration}", matView, sw.Elapsed);
-            }
-        }
-        
-        public async Task RunMigrations(bool repair)
-        {
-            await using var connection = await Obtain();
-            var evolve = new Evolve.Evolve(connection, msg => _logger.Information("Evolve: {Message}", msg))
-            {
-                Locations = new[] {Path.Join(Path.GetDirectoryName(typeof(Database).Assembly.Location), "Schema")}, 
-                IsEraseDisabled = true,
-                SqlMigrationPrefix = "v",
-                SqlRepeatableMigrationPrefix = "r",
-                CommandTimeout = 9999999 // some of these are really long!
-            };
-            
-            if (repair)
-                evolve.Repair();
+        //public async Task RefreshMaterializedViews(NpgsqlConnection conn, params string[] matViews)
+        //{
+        //    foreach (var matView in matViews)
+        //    {
+        //        var sw = new Stopwatch();
+        //        sw.Start();
+        //        await conn.ExecuteAsync($"refresh materialized view concurrently {matView}");
+        //        sw.Stop();
 
-            // Evolve isn't async >.>
-            await Task.Run(() => evolve.Migrate());
-        }
-
-        private class JsonTypeHandler<T> : SqlMapper.TypeHandler<T> where T: JToken
-        {
-            public override void SetValue(IDbDataParameter parameter, T value)
-            {
-                var p = (NpgsqlParameter) parameter;
-                
-                p.Value = value;
-                p.NpgsqlDbType = NpgsqlDbType.Jsonb;
-            }
-
-            public override T Parse(object value)
-            {
-                return JsonConvert.DeserializeObject<T>((string) value);
-            }
-        }
-        
-        private class STJsonTypeHandler : SqlMapper.TypeHandler<JsonElement>
-        {
-            public override void SetValue(IDbDataParameter parameter, JsonElement value)
-            {
-                var p = (NpgsqlParameter) parameter;
-                
-                p.Value = value.GetRawText();
-                p.NpgsqlDbType = NpgsqlDbType.Jsonb;
-            }
-
-            public override JsonElement Parse(object value)
-            {
-                using var doc = JsonDocument.Parse((string) value);
-                return doc.RootElement.Clone();
-            }
-        }
-        
-        private class PassthroughTypeHandler<T>: SqlMapper.TypeHandler<T>
-        {
-            private readonly NpgsqlDbType _type;
-
-            public PassthroughTypeHandler(NpgsqlDbType type)
-            {
-                _type = type;
-            }
-
-            public override void SetValue(IDbDataParameter parameter, T value)
-            {
-                parameter.Value = value;
-                if (parameter is NpgsqlParameter p)
-                    p.NpgsqlDbType = _type;
-            }
-
-            public override T Parse(object value) => (T) value;
-        }
+        //        _logger.Information("Refreshed materialized view {ViewName} in {Duration}", matView, sw.Elapsed);
+        //    }
+        //}
     }
 }
